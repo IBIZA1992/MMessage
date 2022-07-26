5612e07d9fe4c6c8849e350db273fda1919231ab
JiangNan <jn200319@outlook.com>
2022-07-26 10:48:17 +0800

    Initial Commit

---
+++ MMessage/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MMessage
+//
+//  Created by JiangNan on 2022/7/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
